6a85de44a0e04c1d92ef37516490a27c59d3e962
Ezra2005 <muhiziezra2005@gmail.com>
2025-09-15 12:43:57 +0200

    Delete Logger.php.php

--- Logger.php.php
+++
@@ -1,7 +0,0 @@
-class Logger {
-    public static function log($userId, $action) {
-        $db = Database::getInstance();
-        $stmt = $db->prepare("INSERT INTO audit_log (user_id, action) VALUES (?, ?)");
-        $stmt->execute([$userId, $action]);
-    }
-}
